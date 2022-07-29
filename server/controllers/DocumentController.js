8cfb5abcfdf7be091203396b87b8fa064bb570eb
icastro-exactian <icastro@exactian.com>
2022-07-29 19:15:44 -0300

    fc paginated

--- server/controllers/DocumentController.js
+++ server/controllers/DocumentController.js
@@ -1,4 +1,6 @@
+const AreaModel = require("../models/AreaModel");
 const DocumentModel = require("../models/DocumentModel");
+const { PAGE_LIMIT } = process.env;
 exports.create = async (req, res) => {
   const {
     name,
@@ -82,5 +84,35 @@ exports.deleteById = async (req, res) => {
     res.status(200).send("Documento eliminado!");
   } else {
     res.status(500).json({ msg: "Error al eliminar el documento." });
+  }
+};
+const calcNumOffset = (page) => {
+  //calculo el numero del offset
+  let numOffset = (Number(page) - 1) * Number(PAGE_LIMIT);
+  return numOffset;
+};
+const calcTotalPages = (totalItems) => {
+  //Cantidad de paginas en total
+  const cantPages = Math.ceil(totalItems / Number(PAGE_LIMIT));
+  return cantPages;
+};
+exports.getDocumentPaginated = async (req, res) => {
+  try {
+    const { page } = req.params;
+    const offsetIns = calcNumOffset(page);
+    let { count, rows } = await DocumentModel.findAndCountAll({
+      include: [{ model: AreaModel }],
+      offset: offsetIns,
+      limit: Number(PAGE_LIMIT),
+    });
+    const cantPages = calcTotalPages(count);
+    if (rows) {
+      res.status(200).json({ pages: cantPages, response: rows});
+    } else {
+      res.status(500).json({ msg: "Los documentos no existen." });
+    }
+  } catch (e) {
+    console.log(e)
+    return res.status(503).json({ msg: 'Fallo en el servidor' });
   }
 };
--- server/routes/DocumentRouter.js
+++ server/routes/DocumentRouter.js
@@ -4,6 +4,7 @@ const DocumentController = require('../controllers/DocumentController');
 router.post('/create',DocumentController.create);
 router.put('/edit/:id',DocumentController.updateById);
 router.get('/get/:id',DocumentController.getById);
+router.get('/get/documents/:page',DocumentController.getDocumentPaginated);
 router.get('/getall',DocumentController.getAll);
 router.delete('/delete/:id',DocumentController.deleteById);
 
