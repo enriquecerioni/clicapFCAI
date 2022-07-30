f9c8c534ac7ab19aa0bf61946f5dad4568296ef4
Enrque Cerioni <enriquecerioni39@gmail.com>
2022-07-30 11:50:05 -0300

    [add:upload file]

--- server/controllers/DocumentController.js
+++ server/controllers/DocumentController.js
@@ -1,11 +1,58 @@
 const DocumentModel = require("../models/DocumentModel");
+const multer = require('multer');
+const path = require('path');
+
+// Multer Config
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, '../public/documents'),
+  filename:  (req, file, cb) => {
+      cb(null, file.originalname);
+  }
+})
+const uploadDocument = multer({
+  storage,
+  limits: {fileSize: 1000000}
+}).single('urlFile');
+
+
+exports.upload = async (req, res) => {
+
+  uploadDocument(req, res, async (err) => {
+    if (err) {
+        err.message = 'The file is so heavy for my service';
+        return res.send(err);
+    }
+    const {
+      name,
+      areaId,
+      authorId,
+      members,
+      evaluatorId1,
+      evaluatorId2,
+    } = req.body;
+    const doc = await DocumentModel.create({
+      name: name,
+      areaId: areaId,
+      members: members,
+      authorId: authorId,
+      urlFile: req.file.filename,
+      evaluatorId1: evaluatorId1,
+      evaluatorId2: evaluatorId2,
+    });
+    if (doc) {
+      return res.status(200).send("Documento creado!");
+    } else {
+      return res.status(500).json({ msg: "Error al crear el documento." });
+    }
+});
+}
+
 exports.create = async (req, res) => {
   const {
     name,
     areaId,
     authorId,
     members,
-    urlFile,
     evaluatorId1,
     evaluatorId2,
   } = req.body;
@@ -14,7 +61,7 @@ exports.create = async (req, res) => {
     areaId: areaId,
     members: members,
     authorId: authorId,
-    urlFile: urlFile,
+    urlFile: req.file.filename,
     evaluatorId1: evaluatorId1,
     evaluatorId2: evaluatorId2,
   });
--- server/routes/DocumentRouter.js
+++ server/routes/DocumentRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const DocumentController = require('../controllers/DocumentController');
-router.post('/create',DocumentController.create);
+router.post('/create', DocumentController.upload);
 router.put('/edit/:id',DocumentController.updateById);
 router.get('/get/:id',DocumentController.getById);
 router.get('/getall',DocumentController.getAll);
