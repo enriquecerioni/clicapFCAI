2d19712238f0ce6e69b82b3d69ef1d383034d4f4
chino <ivanchinocastro773@gmail.com>
2022-11-26 20:41:45 -0300

    [fixes]

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -1,11 +1,11 @@
 const { PAGE_LIMIT } = process.env;
 
-export const calcNumOffset = (page) => {
+exports.calcNumOffset = (page) => {
   //calculo el numero del offset
   let numOffset = (Number(page) - 1) * Number(PAGE_LIMIT);
   return numOffset;
 };
-export const calcTotalPages = (totalItems) => {
+exports.calcTotalPages = (totalItems) => {
   //Cantidad de paginas en total
   const cantPages = Math.ceil(totalItems / Number(PAGE_LIMIT));
   return cantPages;
