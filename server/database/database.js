bcd575ed6af549e305727d9050921af6a3a56e78
Enrique Cerioni <38478101+enriquecerioni@users.noreply.github.com>
2022-12-26 11:10:14 -0300

    Update database.js
    update db port to deploy

--- server/database/database.js
+++ server/database/database.js
@@ -1,10 +1,11 @@
 const sequelize = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
-const {DATABASE,DATABASE_HOST,DATABASE_USER,DATABASE_PASSWORD} = process.env;
+const {DATABASE,DATABASE_HOST,DATABASE_USER,DATABASE_PASSWORD,DATABASE_PORT} = process.env;
 
 const db = new sequelize(DATABASE, DATABASE_USER, DATABASE_PASSWORD, {
   host: DATABASE_HOST,
+  port: DATABASE_PORT,
   dialect: "mysql",
   define: {
     timestamps: false,
