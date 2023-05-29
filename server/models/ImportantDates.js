15408bc550f29603fc660d668cd40c2d3946c171
chino <ivanchinocastro773@gmail.com>
2023-05-28 23:36:27 -0300

    [fix/ImportantDateModel]: description change for text

--- server/models/ImportantDates.js
+++ server/models/ImportantDates.js
@@ -14,7 +14,7 @@ const ImportantDateModel = db.define("importantDate", {
     allowNull: false
   },
   description: {
-    type: DataTypes.STRING,
+    type: DataTypes.TEXT,
     allowNull: false
   },
 });
